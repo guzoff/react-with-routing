2b1c71b65123d3219be646f50350b31740050172
JeremyHolcomb <jholcomb@kyriba.com>
2018-11-20 15:14:41 -0800

    setup for routing challenge

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import FourOFour from './pages/FourOFour.js';
+import Login from './pages/Login.js';
+import Workspace from './pages/Workspace.js';
+
+/*
+ * Public routes:
+ *   /       - Redirect to login page or workspace depending on authentication status.
+ *   /login  - Login page. Redirect to workspace if authenticated.
+ *   /logout - Remove authentication and redirect to login page with "you have signed out" message.
+ *
+ * Private routes:
+ *   /workspace         - Workspace page. Redirect to login if not authenticated.
+ *   /workspace/subpage - Workspace subpage. Redirect to login if not authenticated.
+ */
 
 class App extends Component {
+  state = {
+    // fake authtoken from sessionStorage
+    authToken: sessionStorage.getItem('authToken')
+  };
+
+  // Usage: this.setAuth(true) or this.setAuth(false)
+  setAuth = value => {
+    value = Boolean(value) || '';
+    sessionStorage.setItem('authToken', value);
+    this.setState({ authToken: value });
+  };
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+        <>
+          <header className="header">
+            {/* Render workspace links when logged in */}
+          </header>
+          <main className="main">
+            {/* Define routes here */}
+          </main>
+        </>
     );
   }
 }
---
+++ src/pages/Workspace.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Workspace() {
+  return (
+    <>
+      <h1>Workspace page</h1>
+      <a href="/workspace/subpage">/workspace/subpage link</a>
+      <button>Click here to log out</button>
+    </>
+  );
+}
